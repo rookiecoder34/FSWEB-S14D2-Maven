46de816bd45ed4c5a20ab81ac4c6e44658012df0
rookiecoder34 <ayaydintolga@gmail.com>
2026-02-05 17:30:23 +0300

    Project completed. Just try test.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,14 +1,14 @@
 package org.example;
 
-import org.example.model.Lamp;
-import org.example.model.LampType;
+import org.example.model.*;
+
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        Lamp lamp = new Lamp(LampType.NEON,true,5);
+        Bedroom bedroom=new Bedroom("Tolga",new Wall("left"),new Wall("right"),new Wall("up"),new Wall("down"),new Ceiling(50,PaintColor.GREEN),
+                new Bed("square",2,3,5,5),new Lamp(LampType.NEON,true,5),new Wardrobe(12,12,13.5),new Carpet(12,15,PaintColor.GREEN));
 
-
-
+        System.out.println(bedroom.getCarpet().getColor());
     }
 }
